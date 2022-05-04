fd1d3a988e5ef73316c744b48b85e4c6bb4d5c2f
Uziel Menezes <uzielcivil@gmail.com>
2022-05-04 08:31:58 -0300

    Alterando o imc do primeiro paciente na tela e adicionados comentários no principal.js

--- js/principal.js
+++ js/principal.js
@@ -1,11 +1,17 @@
-var titulo = document.querySelector(".titulo");
+var titulo = document.querySelector(".titulo"); // buscando a classe título e criando sua variável
 
-titulo.textContent = "Aparecida Nutricionista";
+titulo.textContent = "Aparecida Nutricionista"; // alterando o conteúdo da classe título
 
-var paciente = document.querySelector("#primeiro-paciente");
 
-var primeiroPeso = paciente.querySelector(".info-peso").textContent;
+var paciente = document.querySelector("#primeiro-paciente"); // buscando o ID do primeiro paciente e criando sua variável
 
-var primeiraAltura = paciente.querySelector(".info-altura").textContent;
+var peso = paciente.querySelector(".info-peso").textContent; // buscando o contéudo de peso no id do primeiro paciente e criando sua variável
 
-var primeiroImc = primeiroPeso / (primeiraAltura*primeiraAltura);
+var altura = paciente.querySelector(".info-altura").textContent; // o mesmo para altura
+
+var imc = peso / (altura*altura); // calculando o imc do primeiro paciente e gerando sua variável
+
+
+var infoImc = paciente.querySelector(".info-imc"); // buscando a classe do imc do primeiro paciente e gerando sua variável
+
+infoImc.textContent = imc; // buscando o conteúdo da classe do imc do primeiro paciente e atribuindo o valor do primeiro imc calculado
