c0769a6c28ab38111180ab32e25655881b2230ef
Uziel Menezes <uzielcivil@gmail.com>
2022-05-04 07:30:26 -0300

    Adicionada uma ID para o primeiro paciente da lista e calculado imc através do querySelector

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,11 @@
 var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var primeiroPeso = paciente.querySelector(".info-peso").textContent;
+
+var primeiraAltura = paciente.querySelector(".info-altura").textContent;
+
+var primeiroImc = primeiroPeso / (primeiraAltura*primeiraAltura);
