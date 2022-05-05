7298d37d0793876f52f1c71adc17a8125f0d2e9d
Uziel Menezes <uzielcivil@gmail.com>
2022-05-04 22:55:22 -0300

    Removendo o id do primeiro pacient e adicionando uma função for para calcular todos os imcs, referentes aos pacientes

--- js/principal.js
+++ js/principal.js
@@ -3,29 +3,37 @@ var titulo = document.querySelector(".titulo"); // buscando a classe título e c
 titulo.textContent = "Aparecida Nutricionista"; // alterando o conteúdo da classe título
 
 
-var paciente = document.querySelector("#primeiro-paciente"); // buscando o ID do primeiro paciente e criando sua variável
+// var paciente = document.querySelector("#primeiro-paciente"); buscando o ID do primeiro paciente e criando sua variável
 
-var peso = paciente.querySelector(".info-peso").textContent; // buscando o contéudo de peso no id do primeiro paciente e criando sua variável
+var pacienteTotal = document.querySelectorAll(".paciente"); // buscando todos os elementos com a classe paciente
 
-var altura = paciente.querySelector(".info-altura").textContent; // o mesmo para altura
+for(var i = 0; i < pacienteTotal.length; i++) {
 
-var infoImc = paciente.querySelector(".info-imc"); // buscando a classe do imc do primeiro paciente e gerando sua variável
+	var paciente = pacienteTotal[i];
 
-var pesoValido = true; // iniciação da variável de peso válido para o if
+	var peso = paciente.querySelector(".info-peso").textContent; // buscando o contéudo de peso no id do primeiro paciente e criando sua variável
 
-var alturaValida = true; // mesmo para altura
+	var altura = paciente.querySelector(".info-altura").textContent; // o mesmo para altura
 
-if(peso <= 0 || peso >= 1000){ // condição if para pesos menores ou iguais a 0 ou maiores ou iguais a 1000, informando caso seja inválido
-	pesoValido = false;
-	infoImc.textContent = "Peso inválido!";
-}
+	var infoImc = paciente.querySelector(".info-imc"); // buscando a classe do imc do primeiro paciente e gerando sua variável
 
-if(altura <= 0 || altura >= 3.00){ // mesmo da condição acima, informando caso altura seja inválida
-	alturaValida = false;
-	infoImc.textContent = "Altura Inválida!";
-}
+	var pesoValido = true; // iniciação da variável de peso válido para o if
 
-if(pesoValido && alturaValida){ // condição if para cálculo do imc se tanto o peso como altura forem válidos
-var imc = peso / (altura*altura); // calculando o imc do primeiro paciente e gerando sua variável
-infoImc.textContent = imc; // buscando o conteúdo da classe do imc do primeiro paciente e atribuindo o valor do primeiro imc calculado
+	var alturaValida = true; // mesmo para altura
+
+	if(peso <= 0 || peso >= 1000){ // condição if para pesos menores ou iguais a 0 ou maiores ou iguais a 1000, informando caso seja inválido
+		pesoValido = false;
+		infoImc.textContent = "Peso inválido!";
+	}
+
+	if(altura <= 0 || altura >= 3.00){ // mesmo da condição acima, informando caso altura seja inválida
+		alturaValida = false;
+		infoImc.textContent = "Altura Inválida!";
+	}
+
+	if(pesoValido && alturaValida){ // condição if para cálculo do imc se tanto o peso como altura forem válidos
+	var imc = peso / (altura*altura); // calculando o imc do primeiro paciente e gerando sua variável
+	infoImc.textContent = imc.toFixed(2); // buscando o conteúdo da classe do imc do primeiro paciente e atribuindo o valor do primeiro imc calculado
+	}
+
 }
