ee4b65bae16e9e68a6388f0feae09b27f5bd1c93
Uziel Menezes <uzielcivil@gmail.com>
2022-05-05 20:31:30 -0300

    Adicionando um objeto para obter os valores do paciente, inclusive imc

--- js/form.js
+++ js/form.js
@@ -3,11 +3,10 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event){ // funcão anônima para monitorar o click, no caso, do botão do formulário
 	event.preventDefault();								// fazendo com que o padrão do html não seja executado
 	var form = document.querySelector("#form-adiciona"); // buscando o formulário e os seus valores específicos
-	var nome = form.nome.value;
-	var peso = form.peso.value;
-	var altura = form.altura.value;
-	var gordura = form.gordura.value;
+	// Extraindo informações do paciente do form
+	var paciente = obtemPacienteFormulario(form);
 
+	//  cria a tr e a td do paciente
 	var pacienteTr = document.createElement("tr"); // criando uma tr a partir do click do botão
 
 	var nomeTd = document.createElement("td"); // criando td's para cada parâmetro
@@ -27,7 +26,22 @@ botaoAdicionar.addEventListener("click", function(event){ // funcão anônima pa
 	pacienteTr.appendChild(gorduraTd);
 	pacienteTr.appendChild(imcTd);  
 
+	// adicionando o paciente na tabela
 	var tabela = document.querySelector("#tabela-pacientes"); // buscando o tbody
 
 	tabela.appendChild(pacienteTr);                         // adicionando o tr ao tbody
 });
+
+
+function obtemPacienteFormulario(form){
+
+	var paciente = {
+		nome: form.nome.value,
+		peso: form.peso.value,
+		altura: form.altura.value,
+		gordura: form.gordura.value,
+		imc: calculaImc(form.peso.value, form.altura.value)
+	}
+
+	return paciente;
+}
