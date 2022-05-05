2349103a9d8e4e625e7ebf5e7bb8a39bf6200506
Uziel Menezes <uzielcivil@gmail.com>
2022-05-05 07:42:20 -0300

    Adicionada a funcionalidade que captura os valores recebidos do usuário e transforma em uma nova tr de paciente. O imc será implementado no futuro

--- js/principal.js
+++ js/principal.js
@@ -44,5 +44,33 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
 botaoAdicionar.addEventListener("click", function(event){ // funcão anônima para monitorar o click, no caso, do botão do formulário
 	event.preventDefault();								// fazendo com que o padrão do html não seja executado
-	console.log("oi");
+	var form = document.querySelector("#form-adiciona"); // buscando o formulário e os seus valores específicos
+	var nome = form.nome.value;
+	var peso = form.peso.value;
+	var altura = form.altura.value;
+	var gordura = form.gordura.value;
+
+	var pacienteTr = document.createElement("tr"); // criando uma tr a partir do click do botão
+
+	var nomeTd = document.createElement("td"); // criando td's para cada parâmetro
+	var pesoTd = document.createElement("td");
+	var alturaTd = document.createElement("td");
+	var gorduraTd = document.createElement("td");
+	var imcTd = document.createElement("td");
+
+	nomeTd.textContent = nome;            // trocando os valores dos td's de acordo com o input do usuário
+	pesoTd.textContent = peso;
+	alturaTd.textContent = altura;
+	gorduraTd.textContent = gordura;
+	// imcTd.textContent = imc;        será adicionado no futuro
+
+	pacienteTr.appendChild(nomeTd);    // adicionando os td's ao tr
+	pacienteTr.appendChild(pesoTd);
+	pacienteTr.appendChild(alturaTd);
+	pacienteTr.appendChild(gorduraTd);
+	// pacienteTr.appendChild(imcTd);  será adicionado no futuro
+
+	var tabela = document.querySelector("#tabela-pacientes"); // buscando o tbody
+
+	tabela.appendChild(pacienteTr);                         // adicionando o tr ao tbody
 });
